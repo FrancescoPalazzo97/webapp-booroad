a2f8fd8ad0b734a25862a54401b538defc413d4e
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-06-06 15:07:45 +0200

    [UPD] Create new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DefaultLayout from "./layout/DefaultLayout"
+import HomePage from "./pages/HomePage"
+import TripDetails from "./pages/TripDetails"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route Component={DefaultLayout}>
+          <Route path="/" Component={HomePage} />
+          <Route path=":id" Component={TripDetails} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <nav className="navbar bg-body-tertiary">
+            <div className="container-fluid">
+                <img src="/react.svg" alt="Logo" className="d-inline-block align-text-top" />
+                BooRoad
+            </div>
+        </nav>
+    )
+}
+
+export default Header
